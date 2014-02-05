1f66260a85e29b5095981e99abf80d9e6f0a6574
Patrick Campanale <patrickappcreator@gmail.com>
2014-02-05 08:45:43 -0800

    Carbon bringup
    Change-Id: Iba051d5dedfc61e7d37f64e7edbe58aa4ad03822

--- carbon.mk
+++ carbon.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/samsung/jflteusc/full_jflteusc.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.2.2/JDQ39/R970VXUAMD9:user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.2.2 JDQ39 R970VXUAMD9 release-keys"
 
-PRODUCT_NAME := cm_jflteusc
+PRODUCT_NAME := carbon_jflteusc
 PRODUCT_DEVICE := jflteusc
 
